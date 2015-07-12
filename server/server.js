c09c5246cf0b1ec469bec15a08076633305397e9
coutar-a <AGCoutarel@gmail.com>
2015-07-12 14:30:47 +0200

    ajout de la partie client, WIP

--- server/server.js
+++ server/server.js
@@ -3,33 +3,35 @@ var path = require('path');
 var fs = require('fs');
 var port = process.argv[2];
 var imgStream = fs.createReadStream(process.argv[3]);
-var ext = "image" + path.extname(process.argv[3]);
-ext = ext.replace('.', '/');
+var ext = ("image" + path.extname(process.argv[3])).replace('.','/');
 
 var connections = [];
-var i = 0;
 
 function addIpToList(IpAddress)
 {
     var index = undefined;
 
     for (index = 0 ; connections[index] ; index++) {
-	if (connections[index] === IpAddress)
-	    return false;
+		if (connections[index] === IpAddress)
+			return false;
     }
     connections[index] = IpAddress;
     return true;
 }
 
 var server = http.createServer(function (request, response) {
-    if (connections.length == 0) {
-	addIpToList(request.connection.remoteAddress);
-	//connections[i++] = request.connection.remoteAddress;
-	//console.log(connections[i - 1]);
-	response.writeHead(200, {'Content-type' : ext});
-	imgStream.pipe(response);
+    if (connections.length == 0)
+    {
+		addIpToList(request.connection.remoteAddress);
+		response.writeHead(200, {'Content-type' : ext});
+		imgStream.pipe(response);
     }
-    response.writeHead(200, {'Content-type' : 'text/plain'});
-    imgStream.pipe(connections.join());
+    else
+    {
+		response.writeHead(203, {'Content-type' : 'text/plain'});
+		response.end(connections.join());
+    }
+    for (var index = 0; connections[index] ; index++)
+		console.log(connections[index]);
 });
 server.listen(port);
